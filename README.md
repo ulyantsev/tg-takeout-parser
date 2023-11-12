5802ec8f284ff69b65c74977718176d6da300ba4
ulyantsev <vl.ulyantsev@gmail.com>
2023-11-12 03:09:14 +0300

    script pre-release version

---
+++ README.md
@@ -0,0 +1,23 @@
+
+This is a simple code to extract the dataframe with messages
+from Telegram Desktop exported chat history result.json
+
+More https://github.com/MasterScrat/Chatistics/tree/master
+
+You can add more fields to MESSAGE_RECORDS to extract:
+
+    ['id', 'type', 'date', 'date_unixtime', 'from', 'from_id', 'text',
+       'text_entities', 'file', 'mime_type', 'thumbnail', 'media_type',
+       'duration_seconds', 'width', 'height', 'forwarded_from', 'saved_from',
+       'photo', 'sticker_emoji', 'edited', 'edited_unixtime',
+       'reply_to_message_id', 'actor', 'actor_id', 'action', 'title',
+       'contact_information.first_name', 'contact_information.last_name',
+       'contact_information.phone_number', 'members', 'via_bot',
+       'location_information.latitude', 'location_information.longitude',
+       'place_name', 'address', 'message_id', 'poll.question', 'poll.closed',
+       'poll.total_voters', 'poll.answers', 'live_location_period_seconds',
+       'discard_reason', 'inviter', 'contact_vcard', 'performer',
+       'self_destruct_period_seconds', 'schedule_date', 'duration', 'to',
+       'to_id', 'distance', 'reason_domain', 'game_title', 'game_description',
+       'game_link', 'game_message_id', 'score', 'chat_name', 'chat_id',
+       'chat_type']
--- tg-takeout-parser.py
+++ tg-takeout-parser.py
@@ -1,16 +1,12 @@
 # author: @ulyantsev
 
-import json
 import ijson
 import sys
-import time
-
 import numpy as np
 import pandas as pd
-
-import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
 
 CHAT_TYPES = ['private_group', 'personal_chat']
 
@@ -37,9 +33,16 @@ STAT_FUNCTIONS = {f'{CHAT_RECORDS_PREFIX}id': 'nunique',
 
 STAT_COLUMNS_NAMES = ['chats', 'msg', 'chr', 'media_sec']
 
-def load_selected_msg_data(json_path: str):
+
+def load_needed_chats_data(json_path: str) -> list:
+    """
+    Iteratively loads chats data from telegram takeout json file.
+    This could be replaced with json.load(), if you don't want to use ijson.
+
+    :param json_path: path to telegram takeout json file
+    :return: list of dictionaries with chats data
+    """
     with open(json_path, encoding='utf-8') as result_file:
-        result_file.seek(0)
         objects = ijson.items(result_file, 'chats.list.item')
         chats = (o for o in objects if o['type'] in CHAT_TYPES)
         res = []
@@ -49,24 +52,25 @@ def load_selected_msg_data(json_path: str):
             res.append(c)
         return res
 
-def load_my_id(json_path: str):
+def load_my_id(json_path: str) -> int:
     with open(json_path, encoding='utf-8') as result_file:
         objects = ijson.items(result_file, 'personal_information')
         return next(objects)['user_id']
 
 
-def normalize_text_value(value,
+def normalize_text_value(value: str | list,
                          replace_links=True) -> str:
     """
     In TG takeout the majority of message->text values are just strings,
     but some of them are lists of elements with 'type' field (like links, bold, etc.).
-    So for more accurate statistics we need to normalize them to strings and replace links with placeholders
+    So for more accurate statistics we need to normalize them to strings and replace links with placeholders.
+
     :param value: a string or a list of strings or dicts with 'type' field
     :param replace_links: replace links with placeholders
     :return: a normalized string
     """
 
-    def elem_f(e): return f"<<{e['type']}>>" if (replace_links and e['type'] == 'link') else e['text']
+    def elem_f(e): return f"<<link>>" if (replace_links and e['type'] == 'link') else e['text']
 
     if isinstance(value, list):
         return ' '.join([t if isinstance(t, str) else elem_f(t) for t in value])
@@ -77,32 +81,14 @@ def gen_messages_dataframe(chats_list,
                            chat_types=None,
                            print_stats=False) -> pd.DataFrame:
     """
-    Generate dataframe from telegram takeout json data
+    Generate dataframe from telegram takeout json data->chats->list.
 
-    For my data the set of fields in "messages" is:
-    ['id', 'type', 'date', 'date_unixtime', 'from', 'from_id', 'text',
-       'text_entities', 'file', 'mime_type', 'thumbnail', 'media_type',
-       'duration_seconds', 'width', 'height', 'forwarded_from', 'saved_from',
-       'photo', 'sticker_emoji', 'edited', 'edited_unixtime',
-       'reply_to_message_id', 'actor', 'actor_id', 'action', 'title',
-       'contact_information.first_name', 'contact_information.last_name',
-       'contact_information.phone_number', 'members', 'via_bot',
-       'location_information.latitude', 'location_information.longitude',
-       'place_name', 'address', 'message_id', 'poll.question', 'poll.closed',
-       'poll.total_voters', 'poll.answers', 'live_location_period_seconds',
-       'discard_reason', 'inviter', 'contact_vcard', 'performer',
-       'self_destruct_period_seconds', 'schedule_date', 'duration', 'to',
-       'to_id', 'distance', 'reason_domain', 'game_title', 'game_description',
-       'game_link', 'game_message_id', 'score', 'chat_name', 'chat_id',
-       'chat_type']
-    But only several selected for the resulting dataframe.
-
-    :param jdata:
-    :param chat_meta_prefix:
+    :param chats_list: list of chats
+    :param print_stats: print to console dataframe info and memory usage
     :param chat_types: list of chat types to include in the resulting dataframe
     {'private_group', 'bot_chat', 'public_supergroup', 'saved_messages',
      'private_supergroup', 'private_channel', 'public_channel', 'personal_chat'}
-    :return:
+    :return: dataframe with messages
     """
 
     if chat_types is None:
@@ -117,7 +103,7 @@ def gen_messages_dataframe(chats_list,
     # leaving only selected columns
     chat_columns_types = {f'{CHAT_RECORDS_PREFIX}{k}': v for k, v in CHAT_RECORDS.items()}
     columns_types = {**chat_columns_types, **MESSAGE_RECORDS}
-    df = df.reindex(list(columns_types.keys()))
+    df = df.reindex(list(columns_types.keys()), axis='columns')
 
     df = df[df[f'{CHAT_RECORDS_PREFIX}type'].isin(chat_types)]
 
@@ -156,20 +142,37 @@ def gen_stats_dataframe(df: pd.DataFrame,
     stat_df.columns = STAT_COLUMNS_NAMES
     return stat_df
 
+def gen_sent_received_dataframe(msg_df: pd.DataFrame, my_id) -> pd.DataFrame:
+    sent_df = gen_stats_dataframe(msg_df[msg_df['from_id'] == my_id], exclude_forwarded=True)
+    received_df = gen_stats_dataframe(msg_df[msg_df['from_id'] != my_id])
+    return sent_df.merge(received_df, on='date', suffixes=('_sent', '_received'))
 
-def gen_stat_plotly_fig(df: pd.DataFrame) -> go.Figure:
+
+def gen_stat_plotly_fig(msg_df: pd.DataFrame, my_id: int) -> go.Figure:
     """
     Generate plotly figure from dataframe with messages
-    :param df: dataframe with stats
+
+    :param msg_df: dataframe with messages
+    :param my_id: my id number
     """
-    # sent_df = gen_stats_dataframe(df[df['from_id'] == my_id], exclude_forwarded=True)
+
+    merged_df = gen_sent_received_dataframe(msg_df, my_id)
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-    # fig.add_trace(go.Bar(x=merged_df.index.to_timestamp(), y=merged_df['chr_sent'], name='chr'), secondary_y=False)
-    # fig.add_trace(go.Bar(x=merged_df.index.to_timestamp(), y=merged_df['len_received'], name='received'),
+    fig.add_trace(go.Bar(x=merged_df.index.to_timestamp(), y=merged_df['chr_sent'], name='chr_sent'), secondary_y=False)
+    # fig.add_trace(go.Bar(x=merged_df.index.to_timestamp(), y=merged_df['chr_received'], name='chr_received'),
     #               secondary_y=False)
-    # fig.add_trace(go.Scatter(x=merged_df.index.to_timestamp(), y=merged_df['msg_sent'], name='msg'), secondary_y=True)
+
+    fig.add_trace(go.Scatter(x=merged_df.index.to_timestamp(), y=merged_df['msg_sent'], name='msg'), secondary_y=True)
     return fig
+
+
+def main(path: str) -> None:
+    jdata = load_needed_chats_data(path)
+    my_id = load_my_id(path)
+
+    df = gen_messages_dataframe(jdata, print_stats=True)
+    gen_stat_plotly_fig(df, my_id).show()
 
 
 if __name__ == '__main__':
@@ -177,32 +180,4 @@ if __name__ == '__main__':
         print("Usage: python3 tg-takeout-parser.py <result.json>")
         sys.exit()
 
-    start_time = time.time()
-
-    # with open(sys.argv[1], encoding='utf-8') as result_file:
-    #     jdata = json.load(result_file)
-    with open('x.json', 'w', encoding='utf-8') as f:
-        f.write(str(load_selected_msg_data(sys.argv[1])))
-        print(load_my_id(sys.argv[1]))
-
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    # with open(sys.argv[1], encoding='utf-8') as result_file:
-    #
-    #     # this could be done with iterative parsing, but it's not needed for now
-    #     jdata = json.load(result_file)
-    #
-    #     my_id = jdata['personal_information']['user_id']
-    #
-    #     df = gen_messages_dataframe(jdata)
-    #     print(df.head(15).to_string())
-    #
-    #     sent_df = gen_stats_dataframe(df[df['from_id'] == my_id], exclude_forwarded=True)
-    #     received_df = gen_stats_dataframe(df[df['from_id'] != my_id])
-    #
-    #     merged_df = sent_df.merge(received_df, on='date', suffixes=('_sent', '_received'))
-    #     print(merged_df)
-
-
-
+    main(sys.argv[1])
